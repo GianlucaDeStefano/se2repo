337723eed437a554ed38b976ef88ec5dd6ffb198
Nicholas <nicholas.gobbo@studenti.unitn.it>
2018-12-04 22:42:30 +0100

    Added tests for the /exams path

--- api/exams.test.js
+++ api/exams.test.js
@@ -4,46 +4,61 @@ var version = 'V1';
 
 var host = process.env.BASEURL || 'http://localhost:3000';
 
+//****************** exams path tests ******************
 
+/*
+	- method: GET
+	- path: /exams
+	- valid: yes
+*/
 test("Test GET /exams", async () => {
 
     let response = await fetch(`${host}/${version}/exams`,{
         method: 'GET',
     });
     expect(response.status).toEqual(200);
+	//should also test an empty exams list
 });
 
+/*
+	- method: POST
+	- path: /exams
+	- valid: yes
+*/
 test("Test POST /exams with correct data", async => {
 
 	let exam = {
-		"id": 12,
-		"owner_id": 30;
+		"owner_id": 30,
 		"questions": [
 			{
-				"id": 1;
-				"owner_id": 30;
-				"task_type": MULTIPLE;
-				"text": "The best VCS?";
+				"owner_id": 30,
+				"task_type": MULTIPLE,
+				"text": "The best programming language?",
 				"answers": [
 					{
-						"text": "Git"
-						"correct": false
-					},
-					{
-						"text": "Paper"
-						"correct": false
-					},
-					{
-						"text": "My brain"
+						"text": "C/C++",
 						"correct": true
+					},
+					{
+						"text": "Python",
+						"correct": false
+					},
+					{
+						"text": "Java",
+						"correct": false
 					}
 				]
 			},
 			{
-				"id": 2;
-				"owner_id": 30;
-				"task_type": OPEN;
-				"text": "Why?";
+				"owner_id": 30,
+				"task_type": OPEN,
+				"text": "Why?",
+				"answers": []
+			},
+			{
+				"owner_id": 30,
+				"task_type": OPEN,
+				"text": "Write down a C server, you have 3 minutes.",
 				"answers": []
 			}
 		]
@@ -59,46 +74,21 @@ test("Test POST /exams with correct data", async => {
 
 	expect(response.status).toEqual(200);
 
-	let res = JSON.parse(response.text());
-	expect(res).toIncludeKey('id');
-	expect(res).toIncludeKey('owner_id');
+	let obj = JSON.parse(response.text());
+	expect(obj).toIncludeKey('id');
 });
 
-//the exam's and questions' owner_id aren't the same
-test("Test POST /exams with wrong data", async => {
+/*
+	- method: POST
+	- path: /exams
+	- valid: no
+	- description: trying to create an empty exam
+*/
+test("Test POST /exams with no tasks", async => {
 
 	let exam = {
-		"id": 30,
-		"owner_id": 10;
-		"questions": [
-			{
-				"id": 1;
-				"owner_id": 13;
-				"task_type": MULTIPLE;
-				"text": "The best VCS?";
-				"answers": [
-					{
-						"text": "Git"
-						"correct": false
-					},
-					{
-						"text": "Paper"
-						"correct": false
-					},
-					{
-						"text": "My brain"
-						"correct": true
-					}
-				]
-			},
-			{
-				"id": 2;
-				"owner_id": 14;
-				"task_type": OPEN;
-				"text": "Why?";
-				"answers": []
-			}
-		]
+		"owner_id": 10,
+		"questions": []
 	};
 
 	let response = await fetch('${host}/${version/exams}', {
@@ -111,3 +101,291 @@ test("Test POST /exams with wrong data", async => {
 
 	expect(response.status).toEqual(400);
 });
+
+/*
+	- method: GET
+	- path: /exams/{exam_id}
+	- valid: yes
+	- description: trying to get an existing exam
+*/
+test("Test GET /exams/{exam_id} with right exam_id", async => {
+
+	let exam_id = 1;
+
+	let response = await fetch('${host}/${version}/exams/${exam_id}', {
+		method: 'GET'
+	});
+
+	expect(response.status).toBe(200);
+
+	let obj = JSON.parse(response.text());
+	expect(obj).toIncludeKey('id');
+});
+
+/*
+	- method: GET
+	- path: /exams/{exam_id}
+	- valid: no
+	- description: trying to get an unexisting exam
+*/
+test("Test GET /exams/{exam_id} with wrong exam_id", async => {
+
+	let exam_id = 9999;
+
+	let response = await fetch('${host}/${version}/exams/${exam_id}', {
+		method: 'GET'
+	});
+
+	expect(response.status).toBe(404);
+});
+
+/*
+	- method: PATCH
+	- path: /exams/{exam_id}
+	- valid: yes
+	- description: trying to modify an existing exam
+*/
+test("Test GET /exams/{exam_id} with right exam_id", async => {
+
+	let exam_id = 1;
+	//changed right answer from C to Python
+	let exam_update = {
+		"owner_id": 30,
+		"questions": [
+			{
+				"owner_id": 30,
+				"task_type": MULTIPLE,
+				"text": "The best programming language?",
+				"answers": [
+					{
+						"text": "C/C++",
+						"correct": false
+					},
+					{
+						"text": "Python",
+						"correct": true
+					},
+					{
+						"text": "Java",
+						"correct": false
+					}
+				]
+			},
+			{
+				"owner_id": 30,
+				"task_type": OPEN,
+				"text": "Why?",
+				"answers": []
+			},
+			{
+				"owner_id": 30,
+				"task_type": OPEN,
+				"text": "Write down a Python server, you have 3 minutes.",
+				"answers": []
+			}
+		]
+	}
+
+	let response = await fetch('${host}/${version}/exams/${exam_id}', {
+
+		method: 'PATCH',
+		body: JSON.stringify(exam_update),
+		headers: {
+			'Content-type': 'application/json'
+		}
+	});
+
+	expect(response.status).toBe(200);
+
+	let obj = JSON.parse(response.text());
+	expect(obj).toIncludeKey('id');
+});
+
+/*
+	- method: PATCH
+	- path: /exams/{exam_id}
+	- valid: no
+	- description: trying to modify an unexisting exam
+*/
+test("Test GET /exams/{exam_id} with right exam_id", async => {
+
+	let exam_id = 9999;
+	//changed right answer from C to Python
+	let exam_update = {
+		"owner_id": 30,
+		"questions": [
+			{
+				"owner_id": 30,
+				"task_type": MULTIPLE,
+				"text": "The best programming language?",
+				"answers": [
+					{
+						"text": "C/C++",
+						"correct": false
+					},
+					{
+						"text": "Python",
+						"correct": true
+					},
+					{
+						"text": "Java",
+						"correct": false
+					}
+				]
+			},
+			{
+				"owner_id": 30,
+				"task_type": OPEN,
+				"text": "Why?",
+				"answers": []
+			},
+			{
+				"owner_id": 30,
+				"task_type": OPEN,
+				"text": "Write down a Python server, you have 3 minutes.",
+				"answers": []
+			}
+		]
+	}
+
+	let response = await fetch('${host}/${version}/exams/${exam_id}', {
+
+		method: 'PATCH',
+		body: JSON.stringify(exam_update),
+		headers: {
+			'Content-type': 'application/json'
+		}
+	});
+
+	expect(response.status).toBe(404);
+});
+
+/*
+	- method: DELETE
+	- path: /exams/{exam_id}
+	- valid: yes
+	- description: trying to delete an existing exam
+*/
+test("Test DELETE /exams/{exam_id} with right exam_id", async => {
+
+	let exam_id = 1;
+
+	let response = await fetch('${host}/${version}/exams/${exam_id}', {
+		method: 'DELETE',
+	});
+
+	expect(response.status).toBe(204);
+});
+
+/*
+	- method: DELETE
+	- path: /exams/{exam_id}
+	- valid: no
+	- description: trying to delete an unexisting exam
+*/
+test("Test DELETE /exams/{exam_id} with wrong exam_id", async => {
+
+	let exam_id = 9999;
+
+	let response = await fetch('${host}/${version}/exams/${exam_id}', {
+		method: 'DELETE',
+	});
+
+	expect(response.status).toBe(404);
+});
+
+/*
+	- method: GET
+	- path: /exams/{exam_id}/marks
+	- valid: yes
+	- description: trying to get the marks list of a single exam
+*/
+test("Test GET /exams/{exam_id}/marks with right exam_id", async => {
+
+	let exam_id = 1;
+
+	let response = await fetch('${host}/${version}/exams/{exam_id}/marks', {
+		method: 'GET'
+	});
+
+	expect(response.status).toBe(200);
+
+});
+
+/*
+	- method: GET
+	- path: /exams/{exam_id}/marks
+	- valid: no
+	- description: trying to get the marks list of a single unexisting exam
+*/
+test("Test GET /exams/{exam_id}/marks with wrong exam_id", async => {
+
+	let exam_id = 9999;
+
+	let response = await fetch('${host}/${version}/exams/{exam_id}/marks', {
+		method: 'GET'
+	});
+
+	expect(response.status).toBe(404);
+
+});
+
+/*
+	- method: GET
+	- path: /exams/{owner_id}/owner
+	- valid: yes
+	- description: trying to get the exams list of a single owner
+*/
+test("Test GET /exams/{owner_id}/owner with right owner_id", async => {
+
+	let owner_id = 1;
+
+	let response = await fetch('${host}/${version}/exams/{owner_id}/owner', {
+		method: 'GET'
+	});
+
+	expect(response.status).toBe(200);
+	//should also test when no exams present
+});
+
+/*
+	- method: GET
+	- path: /exams/{owner_id}/owner
+	- valid: no
+	- description: trying to get the exams list of a single unexisting owner
+*/
+test("Test GET /exams/{owner_id}/owner with wrong owner_id", async => {
+
+	let owner_id = 9999;
+
+	let response = await fetch('${host}/${version}/exams/{owner_id}/owner', {
+		method: 'GET'
+	});
+
+	expect(response.status).toBe(404);
+	//should also test when no exams present
+});
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+//******************++++++++++++++++++******************
--- app.js
+++ app.js
@@ -1,15 +1,92 @@
 const express = require('express')
 const app = express()
 const PORT = process.env.PORT || 3000
- 
+
+//---------------------------- IN-MEMORY DATABASE ----------------------------
+
+//*********** EXAMS ***********
+var exams = [
+	//one exam
+	{
+		"id": 1,
+		"owner_id": 10,
+		"questions": [
+			{
+				"id": 1,
+				"owner_id": 10,
+				"task_type": MULTIPLE,
+				"text": "The best VCS?",
+				"answers": [
+					{
+						"text": "Git",
+						"correct": false
+					},
+					{
+						"text": "Paper",
+						"correct": false
+					},
+					{
+						"text": "My brain",
+						"correct": true
+					}
+				]
+			},
+			{
+				"id": 2,
+				"owner_id": 10,
+				"task_type": OPEN,
+				"text": "Why?",
+				"answers": []
+			}
+		]
+	},
+
+	//second exam
+	{
+		"id": 2,
+		"owner_id": 20,
+		"questions": [
+			{
+				"id": 3,
+				"owner_id": 20,
+				"task_type": MULTIPLE,
+				"text": "Calculate 1+1.",
+				"answers": [
+					{
+						"text": "0",
+						"correct": false
+					},
+					{
+						"text": "1",
+						"correct": false
+					},
+					{
+						"text": "2",
+						"correct": true
+					}
+				]
+			},
+			{
+				"id": 4,
+				"owner_id": 20,
+				"task_type": OPEN,
+				"text": "Why?",
+				"answers": []
+			}
+		]
+	}
+
+]
+//*****************************
+
 var courses_offered = [{id: 21, name: 'HCI'},
 {id: 28, name:'sweng'}]
- 
- 
+//----------------------------------------------------------------------------
+
 app.get('/', (req, res) => res.send('Hello World and hello Mars!'))
- 
+
 app.get('/courses', (req, res) => {
    res.json(courses_offered)
 })
- 
+
 app.listen(PORT, () => console.log('Example app listening on port'+ PORT))
