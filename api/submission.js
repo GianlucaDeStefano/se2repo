0bc949eed76f57273b207402ee6a3adbc05d541c
francescotescari <36578133+francescotescari@users.noreply.github.com>
2018-12-08 21:52:40 +0100

    first part of sumbission api and impl fixed

---
+++ api/submission.js
@@ -0,0 +1,60 @@
+const util = require('./utility.js');
+const database = require('./database.js');
+
+
+function isAnswer(x){
+	if (util.isNull(x)){
+		return false;
+	} else {
+		return util.isString(x["text"]) && util.isBoolean(x["correct"]);
+	}
+}
+
+function init(app){
+	const TAG = "SUMBISSION";
+	const TABLE = "submissions";
+	
+	//Create submission object and save it 
+	app.post('/submissions', (req, res) => {
+	   var user_id = req.body["user_id"];
+	   var exam_id = req.body["exam_id"];
+	   var answer = req.body["answer"];
+	   if (!util.isInteger(user_id) || !util.isInteger(exam_id) || !util.isArray(answer)){
+		   util.log(TAG, "Error: object properties wrong: "+util.json(req.body));
+		   return res.status(400).send();
+	   }
+	   for (var i = 0; i<answer.length; ++i){
+		   if (!isAnswer(answer[i])){
+				util.log(TAG, "Error: answer is not valid: "+util.json(answer[i]));
+			    return res.status(400).send();
+		   }
+	   }
+		var id=database.generateId(TABLE);
+		var submission = database.submission(id, user_id, exam_id, answer, -1);
+	   if(!database.insert(TABLE, submission)){
+		   util.log(TAG, "Error: failed to insert in database");
+		   return res.status(500).send();
+	   }
+	   util.log(TAG, "Created: "+util.json(submission));
+	   return res.json(submission);
+	});
+
+	//Get submission object by id
+	app.get('/submissions/:submission_id', (req, res) => {
+	   var submission_id = parseInt(req.params["submission_id"]);
+	   if (!util.isInteger(submission_id)){
+		   util.log(TAG, "Error: id is not an integer: "+submission_id);
+		   return res.status(400).send();
+	   }
+	   var submission = database.findBy(TABLE, "id", submission_id);
+	   if (util.isNull(submission)){
+		   util.log(TAG, "Error: failed to find submission with id "+submission_id+" in database");
+		   return res.status(400).send();
+	   }
+	   util.log(TAG, "Read: "+util.json(submission));
+	   res.json(submission);
+	});
+	
+}
+
+module.exports = {init: init};
---
+++ api/submission.test.js
@@ -0,0 +1,66 @@
+fetch = require('node-fetch');
+const host = process.env.host || 'http://localhost:3000';
+const version = 'V1';
+const path = host; //= ${host}/${version}
+var submission = null;
+test("Test POST /submissions with right data", async () => {
+	let reviewTest = {
+	  "user_id": 2,
+	  "exam_id": 2,
+	  "mark": 10,
+	  "answer": [{"text": "vero", "correct": true},{"text": "La Gioconda", "correct": false},{"text": "Questa è una risponsa ad una domanda aperta", "correct": true}]
+	};
+	let response = await fetch(`${path}/submissions`, {
+		method: 'POST',
+		body: JSON.stringify(reviewTest),
+		headers: {
+			'Content-Type': 'application/json'
+		}
+	});
+	expect(response.status).toEqual(200);
+	submission = await response.json();
+	expect(submission["id"]).toBeDefined();
+});
+test("Test POST /submissions with wrong data", async () => {
+	let reviewTest = {
+	  "id": 10.23,
+	  "user_id": "notarealuserid",
+	  "exam_id": {},
+	  "mark": {"why": "not"}
+	};
+	let response = await fetch(`${path}/submissions`, {
+		method: 'POST',
+		body: JSON.stringify(reviewTest),
+		headers: {
+			'Content-Type': 'application/json'
+		}
+	});
+	expect(response.status).toEqual(400);
+});
+test("Test POST /submissions with wrong anwser data", async () => {
+	let reviewTest = {
+	  "user_id": 2,
+	  "exam_id": 2,
+	  "mark": 10,
+	  "answer": [{"text": 32, "correct": true},{"text": "La Gioconda", "correct": "false"},{"text": "Questa è una risponsa ad una domanda aperta", "correct": "notabool"}]
+	};
+	let response = await fetch(`${path}/submissions`, {
+		method: 'POST',
+		body: JSON.stringify(reviewTest),
+		headers: {
+			'Content-Type': 'application/json'
+		}
+	});
+	expect(response.status).toEqual(400);
+});
+test("Test GET /submissions/{id} with right data", async () => {
+	expect(submission["id"]).toBeDefined();
+	let response = await fetch(`${path}/submissions/`+submission["id"]);
+	expect(response.status).toEqual(200);
+	var result = await response.json();
+	expect(result["id"]).toEqual(submission["id"]);
+});
+test("Test GET /submissions/{id} with wrong data", async () => {
+	let response = await fetch(`${path}/submissions/asd}`);
+	expect(response.status).toEqual(400);
+});
