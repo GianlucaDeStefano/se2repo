183fd369fb1ed076f72a469db5e646c289f16b1b
francescotescari <36578133+francescotescari@users.noreply.github.com>
2018-12-08 18:01:20 +0100

    add log json to utils, fix review swagger

--- api/utility.js
+++ api/utility.js
@@ -1,19 +1,23 @@
+//return true if arg is a string
 function isString(x){
 	if (x == null){
 		return false;
 	}
 	return x.constructor === String;
 }
-
+//return true if arg is null
 function isNull(x){
 	return x == null;
 }
+//return true if arg is an integer
 function isInteger(x){
 	return Number.isInteger(x);
 }
+//return true if arg is an array
 function isArray(x){
 	return Array.isArray(x);
 }
+//delete element (second arg) from array (first arg)
 function arrayDelete(array, el){
 	if (!isArray(array) || isNull(el)){
 		return false;
@@ -26,4 +30,15 @@ function arrayDelete(array, el){
 	return true;
 }
 
-module.exports = {isString:isString, isNull:isNull, isInteger:isInteger, isArray:isArray, arrayDelete:arrayDelete};
+function getTime(){
+	return (new Date()).toJSON().slice(11, 19).replace(/[-T]/g, ':')
+}
+//logs to stdout with time, tag (first arg) and actual log (second arg)
+function log(tag, value){
+	console.log("[%s][%s] %s", getTime(), tag, value);
+}
+//return json string of object
+function json(obj){
+	return JSON.stringify(obj);
+}
+module.exports = {isString:isString, isNull:isNull, isInteger:isInteger, isArray:isArray, arrayDelete:arrayDelete, log: log, json : json};
