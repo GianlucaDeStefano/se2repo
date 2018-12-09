e33dcb41de94a746de3330698cbd6ba6e4df113d
francescotescari <36578133+francescotescari@users.noreply.github.com>
2018-12-09 15:56:42 +0100

    versioned app.js and other tweaks and comments

--- app.js
+++ app.js
@@ -1,15 +1,51 @@
-const express = require('express')
-const app = express()
+//Libraries
+const express = require('express');
+const bodyParser = require('body-parser');
+const util = require('./api/utility.js');
+//Environment
 const PORT = process.env.PORT || 3000
- 
-var courses_offered = [{id: 21, name: 'HCI'},
-{id: 28, name:'sweng'}]
- 
- 
-app.get('/', (req, res) => res.send('Hello World and hello Mars!'))
- 
-app.get('/courses', (req, res) => {
-   res.json(courses_offered)
-})
- 
-app.listen(PORT, () => console.log('Example app listening on port'+ PORT))
+
+//Api init imports. Aggiungere qui import ai file delle implemetazioni
+//const reviewApi = require('./api/review.js');
+//const submissionApi = require('./api/submission.js');
+//const groupApi = require('./api/group.js');
+
+//Versioned flag. true = api on http://localhost/V1 , false = api on http://localhost
+const versioned = true;
+
+//Create app
+const app = express();
+//Use bodyParser
+app.use(bodyParser.urlencoded({ extended: false }))
+app.use(bodyParser.json())
+
+//routerToInit = app if not versioned, versionV1 if versioned;
+var routerToInit = app;
+if (versioned){
+	//Create V1 router
+	const versionV1 = express.Router();
+	routerToInit = versionV1;
+	//User api router on /V1 path 
+	app.use('/V1', versionV1);
+}
+
+//Log all the http requests (method + url) to the api router
+routerToInit.use(function(req, res, next) {
+  util.log("API",req.method+" "+getFullUrl(req));
+  next();
+});
+
+//Init router with api request. Aggiungere qui tutti gli init alle api che volete aggiugnere.
+//reviewApi.init(routerToInit);
+//submissionApi.init(routerToInit);
+//groupApi.init(routerToInit);
+
+//Set the home response
+app.get('/', (req, res) => res.send('Welcome to the most beautiful api ever'));
+//Start the server on port
+app.listen(PORT, () => console.log('Starting server app on port '+ PORT))
+
+//Get full request url from request object
+function getFullUrl(req){
+	return req.protocol + '://' + req.get('host') + req.originalUrl;
+}
