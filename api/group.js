f40c53dccfd071b6b74b68c479ea52fc79be1cb3
francescotescari <36578133+francescotescari@users.noreply.github.com>
2018-12-09 22:51:23 +0100

    added post and get impl and tests

--- api/group.js
+++ api/group.js
@@ -5,6 +5,54 @@ const database = require('./database.js');
 function init(app){
 	const TAG = "GROUP";
 	const TABLE = "groups";
+	
+	//Create group with exams, owner and users
+	app.post('/groups/', (req, res) => {
+		var owner_id = req.body["owner_id"];
+		var exam_ids = req.body["exam_ids"];
+		var user_ids = req.body["user_ids"];
+		if (!util.isInteger(owner_id) || !util.isArray(exam_ids) || !util.isArray(user_ids)){
+			util.log(TAG, "Error: wrong post data: "+util.json(req.body));
+			return res.status(400).send();
+		}
+		for (var i = 0; i<exam_ids.length; ++i){
+			if (!util.isInteger(exam_ids[i])){
+				util.log(TAG, "Error: exam_id not an integer: "+exam_ids[i]);
+			return res.status(400).send();
+			}
+		}
+		for (var i = 0; i<user_ids.length; ++i){
+			if (!util.isInteger(user_ids[i])){
+				util.log(TAG, "Error: user_id not an integer: "+user_ids[i]);
+				return res.status(400).send();
+			}
+		}
+		var id = database.generateId(TABLE);
+		var group = database.group(id, owner_id, exam_ids, user_ids);
+		if (!database.insert(TABLE, group)){
+			util.log(TAG, "Error: cannot insert group into database: "+util.json(group));
+			return res.status(500).send();
+		}
+		util.log(TAG,"Created with id: "+id);
+		return res.json(group);
+	});
+	
+	//Get group by group_id
+	app.get('/groups/:group_id', (req, res) => {
+	   var group_id = parseInt(req.params["group_id"]);
+	   if (!util.isInteger(group_id)){
+		   util.log(TAG, "Error: group_id is not an integer: "+group_id);
+		   return res.status(400).send();
+	   }
+	   var group = database.findBy(TABLE, "id", group_id);
+	   if (util.isNull(group)){
+		   util.log(TAG, "Error: failed to find group with id "+group_id+" in database");
+		   return res.status(404).send();
+	   }
+	   util.log("Read: "+util.json(group));
+	   return res.json(group); 
+	});
+	
 
 	//Add exam_id to group
 	app.put('/groups/:group_id/exam', (req, res) => {
--- api/group.test.js
+++ api/group.test.js
@@ -1,10 +1,76 @@
 const fetch = require('node-fetch');
-const db = require('./database.js');
 const util = require('./utility.js');
 const host = process.env.host || 'http://localhost:3000';
 const version = 'V1';
 const path = `${host}/${version}`;
-var group = db.group(1,[1,2],[2,3,4,5]); //=null  //Questo dovrebbe essere l'oggetto group usato per il test, quello creato con il post e quindi andrebbe assegnato nel test per il post, da mettere come primo test;
+var group = null;
+
+test("Test POST /groups with right data", async () => {
+	let groupData = {
+		"owner_id" : 1,
+		"exam_ids" : [1,2],
+		"user_ids" : [2,3,4,5]	
+	}
+	let response = await fetch(`${path}/groups`, {
+		method: 'POST',
+		body: util.json(groupData),
+		headers: {
+			'Content-Type': 'application/json'
+		}
+	});
+	expect(response.status).toEqual(200);
+	group = await response.json();
+	expect(group["id"]).toBeDefined();
+});
+test("Test POST /groups with wrong data", async () => {
+	let groupData = {
+		"owner_id" : 1,
+		"exam_ids" : 2,
+		"user_ids" : "wow"	
+	}
+	let response = await fetch(`${path}/groups`, {
+		method: 'POST',
+		body: util.json(groupData),
+		headers: {
+			'Content-Type': 'application/json'
+		}
+	});
+	expect(response.status).toEqual(400);
+});
+test("Test POST /groups with wrong ids", async () => {
+	let groupData = {
+		"owner_id" : 1,
+		"exam_ids" : [true,"sda"],
+		"user_ids" : [0,{}]
+	}
+	let response = await fetch(`${path}/groups`, {
+		method: 'POST',
+		body: util.json(groupData),
+		headers: {
+			'Content-Type': 'application/json'
+		}
+	});
+	expect(response.status).toEqual(400);
+});
+test("Test GET /groups/{id} with right data", async () => {
+	expect(group["id"]).toBeDefined();
+	let response = await fetch(`${path}/groups/`+group["id"]);
+	expect(response.status).toEqual(200);
+	let getGroup = await response.json();
+	expect(getGroup["id"]).toEqual(group["id"]);
+});
+
+test("Test GET /groups/{id} with wrong data", async () => {
+	let response = await fetch(`${path}/groups/asd`);
+	expect(response.status).toEqual(400);
+});
+
+test("Test GET /groups/{id} with wrong data id", async () => {
+	let response = await fetch(`${path}/groups/643634`);
+	expect(response.status).toEqual(404);
+});
+
+
 
 test("Test PUT /groups/{id}/exam with right data", async () => {
 	expect(group["id"]).toBeDefined();
