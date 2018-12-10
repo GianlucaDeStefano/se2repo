0d8132ba5e8e1eb573317b2889ffee0232754e2a
gian de stefano <gianlucadestefano@studenti.unitn.it>
2018-12-10 11:30:44 +0100

    fixed nomenclature in submission.test.js

--- api/submission.test.js
+++ api/submission.test.js
@@ -4,15 +4,15 @@ const version = 'V1';
 const path = host+"/"+version;
 var submission = null;
 test("Test POST /submissions with right data", async () => {
-	let reviewTest = {
+	let subTest = {
 	  "user_id": 2,
 	  "exam_id": 2,
 	  "mark": 10,
-	  "answer": [{"text": "vero", "correct": true},{"text": "La Gioconda", "correct": false},{"text": "Questa è una risponsa ad una domanda aperta", "correct": true}]
+	  "answers": [{"text": "vero", "correct": true},{"text": "La Gioconda", "correct": false},{"text": "Questa è una risponsa ad una domanda aperta", "correct": true}]
 	};
 	let response = await fetch(`${path}/submissions`, {
 		method: 'POST',
-		body: JSON.stringify(reviewTest),
+		body: JSON.stringify(subTest),
 		headers: {
 			'Content-Type': 'application/json'
 		}
@@ -22,7 +22,7 @@ test("Test POST /submissions with right data", async () => {
 	expect(submission["id"]).toBeDefined();
 });
 test("Test POST /submissions with wrong data", async () => {
-	let reviewTest = {
+	let subTest = {
 	  "id": 10.23,
 	  "user_id": "notarealuserid",
 	  "exam_id": {},
@@ -30,7 +30,7 @@ test("Test POST /submissions with wrong data", async () => {
 	};
 	let response = await fetch(`${path}/submissions`, {
 		method: 'POST',
-		body: JSON.stringify(reviewTest),
+		body: JSON.stringify(subTest),
 		headers: {
 			'Content-Type': 'application/json'
 		}
@@ -38,15 +38,15 @@ test("Test POST /submissions with wrong data", async () => {
 	expect(response.status).toEqual(400);
 });
 test("Test POST /submissions with wrong anwser data", async () => {
-	let reviewTest = {
+	let subTest = {
 	  "user_id": 2,
 	  "exam_id": 2,
 	  "mark": 10,
-	  "answer": [{"text": 32, "correct": true},{"text": "La Gioconda", "correct": "false"},{"text": "Questa è una risponsa ad una domanda aperta", "correct": "notabool"}]
+	  "answers": [{"text": 32, "correct": true},{"text": "La Gioconda", "correct": "false"},{"text": "Questa è una risponsa ad una domanda aperta", "correct": "notabool"}]
 	};
 	let response = await fetch(`${path}/submissions`, {
 		method: 'POST',
-		body: JSON.stringify(reviewTest),
+		body: JSON.stringify(subTest),
 		headers: {
 			'Content-Type': 'application/json'
 		}
